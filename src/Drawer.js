25c689d10a65bc6f712ad5a95444741fa1fbfe80
Simple Signer <engineering@simplesigner.com>
2021-05-05 09:24:44 -0400

    Handle className, Spread prop on Component

--- src/Drawer.js
+++ src/Drawer.js
@@ -3,61 +3,41 @@ import React from 'react';
 
 // TODO: Change `sidebar-` to `react-bootstrap-drawer-`
 export const DrawerToC = (props) => {
+	const { className, ...other } = props;
+	const classNameActual = `sidebar-toc pt-2 pb-4 ${className}`.trim();
+
 	return (
-		<div className="sidebar-toc pt-2 pb-4">
+		<div
+			{ ...other }
+			className={ classNameActual }>
 			{ props.children }
 		</div>
 	);
 };
 
 export const DrawerOverflow = (props) => {
+	const { className, ...other } = props;
+	const classNameActual = `sidebar-overflow ${className}`.trim();
+
 	return (
-		<div className="sidebar-overflow">
+		<div
+			{ ...other }
+			className={ classNameActual }>
 			{ props.children }
 		</div>
 	);
 };
 
 export const Drawer = (props) => {
-	const className = `sidebar-panel d-flex flex-column ${props.className}`.trim();
+	const { className, ...other } = props;
+	const classNameActual = `sidebar-panel d-flex flex-column ${className}`.trim();
 
 	return (
-		<div className={ className }>
+		<div
+			{ ...other }
+			className={ classNameActual }>
 			{ props.children }
 		</div>
 	);
 };
-
-
-/*
-const Drawer = (props) => {
-	const [open, setOpen] = useState(false);
-
-	const handleToggle = () => setOpen(!open);
-
-	return (
-		<>
-			<div className={ `sidebar-panel d-flex flex-column ${props.className}` }>
-				<Button size="lg" variant="link" className="sidebar-toggle p-0 d-md-none ml-3" onClick={ handleToggle }>
-					<List />
-				</Button>
-
-				<Collapse in={ open }>
-					<div className="sidebar-overflow">
-						<div className="sidebar-toc pt-2 pb-4">
-							<CoreNavigation />
-
-							<SettingNavigation />
-
-							<Restrict role={ UserRole.APPLICATION_ADMINISTRATOR }>
-								<AdministratorNavigation />
-							</Restrict>
-						</div>
-					</div>
-				</Collapse>
-			</div>
-		</>
-	);
-};
-*/
 
--- src/DrawerNavigation.js
+++ src/DrawerNavigation.js
@@ -2,14 +2,26 @@
 import React from 'react';
 
 export const DrawerNavigationHeader = (props) => {
+	const { className, ...other } = props;
+	const classNameActual = `sidebar-toc-header nav-link ${className}`.trim();
+
 	return (
-		<a className="sidebar-toc-header nav-link">{ props.children }</a>
+		<a
+			{ ...other }
+			className={ classNameActual }>
+			{ props.children }
+		</a>
 	);
 };
 
 export const DrawerNavigation = (props) => {
+	const { className, ...other } = props;
+	const classNameActual = `d-block nav ${className}`.trim();
+
 	return (
-		<div className="nav d-block">
+		<div
+			{ ...other }
+			className={ classNameActual }>
 			{ props.children }
 		</div>
 	);
